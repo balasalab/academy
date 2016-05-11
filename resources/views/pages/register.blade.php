7649371b7eb313c6c11655c7b88ef7c47ce002ce
balasalab <blsubramanian15@gmail.com>
2016-05-11 22:18:31 +0530

    correction

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -148,7 +148,7 @@
 			<div class="col-md-6" id="alert-messages">
 				<div class="alert alert-info alert-dismissible" role="alert">
 				  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				  <strong>Welcome KleverKid!</strong>
+				  <strong>Welcome!</strong>
 				</div>
 			</div>     
 	    </div>
